91b2b4267d2fa7bab3383a8539649f5860c275f4
530604689@qq.com <530604689@qq.com>
2019-01-22 17:05:42 +0800

    提供中英文切换接口，完成单向数据绑定

--- languageData.js
+++ languageData.js
@@ -1,22 +1,21 @@
-function getLanguage(moduleName, lan) {
+function getLanguage(moduleName) {
 	var data = {};
 	if ('module1' == moduleName) {
 		data = {
 			lab: {
-				a: ['Flicker frequency:', '防闪频率:'][lan],
-				b: ['WDR:', '宽动态:'][lan],
-				c: ['FaceTargetBrightness:', '人脸目标亮度:'][lan],
-				d: ['FaceExposureInterval(s):', '人脸曝光间隔(s):'][lan],
-				e: ['IlluminationCondition:', '光照条件:'][lan],
-				f: ['HLC:', '背光补偿:'][lan]
+				a: ['Flicker frequency:', '防闪频率:'],
+				b: ['WDR:', '宽动态:'],
+				c: ['FaceTargetBrightness:', '人脸目标亮度:'],
+				d: ['FaceExposureInterval(s):', '人脸曝光间隔(s):'],
+				e: ['IlluminationCondition:', '光照条件:'],
+				f: ['HLC:', '背光补偿:']
 			},
 			sel: {
-				g: ['Auto', '自动'][lan],
-				h: ['Manual', '手动'][lan]
+				g: ['Auto', '自动'],
+				h: ['Manual', '手动']
 			}
 		}
 	}
-
 	return data;
 
 }
--- languageRender.js
+++ languageRender.js
@@ -2,7 +2,7 @@
 
 	function languageRender(option) {
 		this.option = option;
-		this.data = option.data;
+		this.lan = option.lan;
 		this.beforeRender = option.beforeRender || function() {};
 		//判断el是否是数组，即判断是否需要渲染多个包裹元素下的dom
 		if (Object.prototype.toString.call(option.el) == '[object Array]') {
@@ -10,11 +10,30 @@
 		} else {
 			this.el = document.getElementById(option.el);
 		}
+		this.dep = new Dep();
 		this.beforeRender();
+		this.dataInit();
 		this.render();
 	}
 
 	languageRender.prototype = {
+		//由于传进来的文字信息是数组，所以这里需要对数组根据this.lan值进行选择语言，并赋值给this.data
+		dataInit: function() {
+			var o = JSON.parse(JSON.stringify(this.option.data)); //深拷贝对象，避免污染变量，方便后面切换中英文使用
+			this.dataProcessing(o);
+			this.data = o;
+		},
+		dataProcessing: function(data) {
+			for (key in data) {
+				var item = data[key];
+				if (Object.prototype.toString.call(item) == '[object Array]') {
+					data[key] = item[this.lan];
+
+				} else if (Object.prototype.toString.call(item) == '[object Object]') {
+					this.dataProcessing(item);
+				}
+			}
+		},
 		render: function() {
 			if (this.elArray) {
 				for (var i = 0; i < this.elArray.length; i++) {
@@ -70,49 +89,98 @@
 				var dataValue;
 				var self = this;
 				if (self.isDirective(attrName)) { //判断是否是自定义属性 inf-开头
+					/*例如:
+					inf-text='lab.name'  lab:{name:'liuzj'}
+					attrName为inf-text
+					attrValue为lab.name
+					_attrName为text
+					dataKey为lab->name
+					dataValue为liuzj
+					*/
+
 					var _attrName = attrName.slice(4);
 					var _attrValueArray = attrValue.split('.');
 					var len = _attrValueArray.length;
-
-					if (len > 1) { //说明有对象嵌套例如a.b
-						dataValue = self.data;
-						for (var j = 0; j < len; j++) {
-							dataKey = _attrValueArray[j];
-							dataValue = dataValue[dataKey];
-						}
-					} else {
-						dataValue = self.data[attrValue];
+					dataValue = self.data;
+					for (var j = 0; j < len; j++) {
+						dataKey = _attrValueArray[j];
+						dataValue = dataValue[dataKey];
 					}
-
 					if ('text' == _attrName) { //inf-text为设置改节点内包含的文本节点值,类比jq的$().text
-						if (node.textContent != undefined) {
-							node.textContent = dataValue;
-						} else {
-							node.innerText = dataValue; //IE8
-						}
+						self.updateText(node, attrValue, dataValue)
 					} else {
-						self.updateStyle(node, _attrName, dataValue);
+						self.updateStyle(node, _attrName, attrValue, dataValue);
 					}
 					node.removeAttribute(attrName);
 				}
 			}
 		},
-		updateStyle: function(node, attr, val) {
+		updateText: function(node, attrValue, text) {
+			this.updateTextCallBack(node, attrValue, text);
+			new Watcher(this, attrValue, function(value) {
+				this.updateTextCallBack(node, attrValue, value);
+			});
+
+		},
+		//将对node的具体操作单独作为一个callback拿出来，避免多次new Watcher，每一个自定义属性只new一个Watcher
+		updateTextCallBack: function(node, attrValue, text) {
+			if (node.textContent != undefined) {
+				node.textContent = text;
+			} else {
+				node.innerText = text; //兼容IE8
+			}
+		},
+
+		updateStyle: function(node, attr, key, val) {
+			this.updateStyleCallBack(node, attr, key, val);
+			new Watcher(this, key, function(value, oldValue) {
+				this.updateStyleCallBack(node, attr, key, value, oldValue);
+			});
+
+		},
+		updateStyleCallBack: function(node, attr, key, val, oldValue) {
 			if ('class' == attr) {
 				if (node.classList) {
 					node.classList['add'](val);
+					node.classList['remove'](oldValue);
 				} else { //兼容IE8
+					node.className = node.className.replace(oldValue, '');
 					node.className += ' ' + val;
 				}
 			} else {
 				node.setAttribute(attr, val);
+
 			}
 		},
+		//判断是否是元素节点
 		isElementNode: function(node) {
 			return node.nodeType == 1;
 		},
+		//判断是否是自定义属性，即指令
 		isDirective: function(attr) {
 			return attr.indexOf('inf-') == 0;
+		},
+		//单独修改某个数据所提供的接口,例如languageRenderCtx.set('languageData.lab.a','xxx');
+		set: function(key, val) {
+			var keyArray = key.split('.');
+			var data = this.data;
+			for (var i = 0; i < keyArray.length; i++) {
+				if (i < keyArray.length - 1) {
+					data = data[keyArray[i]];
+				} else {
+					data[keyArray[i]] = val;
+				}
+			}
+			this.dep.notify();
+		},
+		//修改全部语言选择所提供的接口 lan:0英文 lan:1中文
+		setLan: function(lan) {
+			if (this.lan == lan) {
+				return
+			}
+			this.lan = lan;
+			this.dataInit();
+			this.dep.notify();
 		}
 	}
 
@@ -129,6 +197,55 @@
 		}
 		return array;
 	}
+
+	// 消息订阅器Dep，订阅器Dep主要负责收集订阅者，然后再属性变化的时候执行对应订阅者的更新函数
+	function Dep() {
+		this.subs = [];
+	}
+
+	Dep.prototype = {
+		addSub: function(sub) {
+			this.subs.push(sub);
+		},
+		// 通知订阅者数据变更
+		notify: function() {
+			for (var i = 0; i < this.subs.length; i++) {
+				this.subs[i].update();
+			}
+		}
+	};
+
+	//订阅者Watcher，每一个节点上的每一个自定义属性就是一个订阅者，等待Dep通知，执行cb从而更新ui
+	function Watcher(languageRenderCtx, key, cb) {
+		this.languageRenderCtx = languageRenderCtx;
+		this.cb = cb;
+		this.key = key;
+		this.value = this.get();
+		this.languageRenderCtx.dep.addSub(this);
+	}
+
+	Watcher.prototype = {
+		update: function() {
+			this.run();
+		},
+		run: function() {
+			var value = this.get();
+			var oldVal = this.value;
+			if (value !== oldVal) {
+				this.value = value;
+				this.cb.call(this.languageRenderCtx, value, oldVal);
+			}
+		},
+		get: function() {
+			var keyArray = this.key.split('.')
+			var value = this.languageRenderCtx.data;
+			for (var i = 0; i < keyArray.length; i++) {
+				value = value[keyArray[i]]
+			}
+			return value;
+		}
+	};
+
 
 	languageRender.prototype.constructor = languageRender;
 	window.languageRender = languageRender;
